8fedeb9cd5aecc4ee6fbe5559ebf0ab479be1a50
Basile B <r0658350@kuleuven.be>
2021-03-25 16:19:13 +0100

    code documentatie

--- Software/Basile/CoolTeam indu prototype/src/main.cpp
+++ Software/Basile/CoolTeam indu prototype/src/main.cpp
@@ -1,19 +1,24 @@
 #include <Arduino.h>
 #include <sSense-HDC2010.h>
 
+
+//I2C Addres van temp sesnor
 #define ADDR 0x40
 
+//Pinnen definen van hall sensor
 #define hallDisablePin GPIO_NUM_4
 #define hallOutPin GPIO_NUM_36
 
+//Init temp sensor object en data var's
 HDC2010 sensor(ADDR);
 float temperature = 0, humidity = 0;
 
+//RTC geheugen var om bij te houden als het systeem uit deepsleep kwam
 RTC_DATA_ATTR int bootCount = 0;
 
+//Testfunctie die alle registers van de temp sensor print
 void printRegisters()
 {
-
   for (size_t i = 0; i < 16; i++)
   {
     Serial.print("Register ");
@@ -23,6 +28,8 @@ void printRegisters()
   }
 }
 
+
+//setup indien systeem volledig herstart is
 void firstBoot()
 {
   sensor.reset();
@@ -41,6 +48,8 @@ void firstBoot()
   Serial.println("First boot!");
 }
 
+
+//Setup als systeem uit deepsleep komt
 void wakeUpRoutine()
 {
   temperature = sensor.readTemp();
@@ -51,40 +60,45 @@ void wakeUpRoutine()
   Serial.println(humidity);
 }
 
+//Methode die de hallsensor uitleest
 void readHallSensor(){
   uint16_t hallDrempelL = 1000;
   uint16_t hallDrempelH = 200;
-  pinMode(hallDisablePin, OUTPUT);
-  gpio_hold_dis(hallDisablePin);
-  int hallData = analogRead(hallOutPin);
-  digitalWrite(hallDisablePin, HIGH);
-  gpio_deep_sleep_hold_en();
-  gpio_hold_en(hallDisablePin);
+
+  
+  pinMode(hallDisablePin, OUTPUT);        //Pin als output zetten
+  gpio_hold_dis(hallDisablePin);          // Disable hold functie -> sensor komt in active mode
+  int hallData = analogRead(hallOutPin);  //Lees analoge waarde van sensor
+  digitalWrite(hallDisablePin, HIGH);     //Zet de enable pin van de sensor hoog -> sensor komt in slaap mode
+  gpio_deep_sleep_hold_en();              //Enable de hold functie tijdens deepsleep
+  gpio_hold_en(hallDisablePin);           //Enable de hold functie voor bepaalde pin -> voorkomt dat pin laag komt als de esp slaapt!!
+
+  //Threshold logica
   if (hallData <= hallDrempelL || hallData >=hallDrempelH)
     Serial.println("Deur open!!");
 }
 
+
+//Main setup
 void setup()
 {
-
   Serial.begin(115200);
   while (!Serial)
     ;
   sensor.begin();
 
+  //Logica die kijkt indien het systeem uit slaap komt of als het voor het eerst start
   if (bootCount == 0)
     firstBoot();
   else
     wakeUpRoutine();
 
-
   readHallSensor();
 
-  esp_sleep_enable_ext0_wakeup(GPIO_NUM_25, 1);
+  esp_sleep_enable_ext0_wakeup(GPIO_NUM_25, 1);   //Selecteer wake-up pin
   Serial.println("Going to sleep now");
+  //Slaap
   esp_deep_sleep_start();
-
-  //printRegisters();
 }
 
 void loop()
